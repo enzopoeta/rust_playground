9cdfa2a8819f5dd32a8848936dee0e28a2741b3a
enzo <enzopoeta@gmail.com>
2020-03-09 21:38:38 -0300

    continuando exercicios de structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -7,6 +7,16 @@ struct User {
     active: bool,
 }
 
+/// também é possiível adicionar funcões que operam nas instancias de uma structs 
+/// isso é feito com a declaracao impl congforme o exemplo abaixo
+/// o primeiro parametro de funcao sempre deve ser a referencia a propria struct (&self)
+impl User {
+    fn get_email_info(&self) -> String {
+        self.email.clone()
+    }
+}
+
+
 
 // se temos uma funcao que utiliza parametros com nomes iguais aos atributos da sequence nao precisamos seta-los especificamente ao 
 // fazer a atribuicao como podemos verificar no exemplo abaixo
@@ -21,6 +31,9 @@ fn build_user(email: String, username: String) -> User {
 
 
 fn main() {
+
+
+
     
     //declaracao de uma struct especifica
     let mut user1 = User {
@@ -40,4 +53,41 @@ fn main() {
     println!("acessando o email do usuario apos alteracao {}\n",user1.email);
 
 
+
+    // tambem é possivel copiar os valores de uma struct utilizando a seguinte sintaxe
+    let user2 = User {
+        email: String::from("another@example.com"), // email e username sao campos que desejamos colocar valores especificos
+        username: String::from("anotherusername567"),
+        ..user1 // o resto sera copiado do user 2
+    };
+
+    println!("printando o usuario 2 --> {:?}",user2);
+
+    
+    // existe tambem a possibilidade de utilizarmos a construção de structs sem nomes de atributos definidos (tuplas tipadas)
+    // conformme os exemplos abaixo
+    #[derive(Debug)]
+    struct Color(i32, i32, i32);
+    #[derive(Debug)]
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    println!("printando A COR PRETA --> {:?}",black);
+
+    println!("printando o ponto origem --> {:?}",origin);
+
+    // e possivel inclusive criar uma tupla sem quaisquer campos ! isso pode ser util quando vc precisar criar uma trait por exemplo 
+    // (vc precisa criar um tipo novo e nao precisa passar nenhum atributo p ele)
+
+    /*
+            #-- Structs e ownership  --#
+
+            Em principio nao eh possivel passar valores de struct por referencia , porque pode ser que em algum momento o referido dado saia de escopo
+            é possivel fazer isso se for definido um lifetime para o mesmo  (esse processo sera estudado mais a frente)
+    */
+    
+    /// chamando metodos de uma struct 
+    println!("printando o email pelo metodo de instancia da struct User -> {}",user1.get_email_info());
 }
