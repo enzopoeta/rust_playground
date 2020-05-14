ee16c87e5f5622770ebee0dbb3b344132c5e11ec
enzo <enzopoeta@gmail.com>
2020-05-14 20:15:52 -0300

    removendo binarios do projeto de collections

--- collections_stdlib/target/debug/collections_stdlib.d
+++
@@ -1,1 +0,0 @@
-/home/enzo/rust_playground/collections_stdlib/target/debug/collections_stdlib: /home/enzo/rust_playground/collections_stdlib/src/main.rs
--- collections_stdlib/target/debug/deps/collections_stdlib-4c7dcb8cd97591cd.d
+++
@@ -1,5 +0,0 @@
-/home/enzo/rust_playground/collections_stdlib/target/debug/deps/collections_stdlib-4c7dcb8cd97591cd: src/main.rs
-
-/home/enzo/rust_playground/collections_stdlib/target/debug/deps/collections_stdlib-4c7dcb8cd97591cd.d: src/main.rs
-
-src/main.rs:
