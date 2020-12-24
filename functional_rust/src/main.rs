ad35001894dc33ee8b11c5ae467161c2035242cd
enzo <enzopoeta@gmail.com>
2020-12-24 13:30:46 -0300

    adicionando exemplos de api funcional com colecoes no projeto functional_rust

--- functional_rust/src/main.rs
+++ functional_rust/src/main.rs
@@ -98,14 +98,57 @@ fn main() {
     
     println!("Nova posicao do personagem depois do char_up_action : {:?}",char_position);
 
+    
+    
+    // ITERATORS / ADAPTERS / Consumers
+
+    // para criar um iterator em uma estrutura que  implementa a trait  Iterator podemos utilizar
+    // .iter() para obter um iterador por referencia e .into_iter() para obter um iterador com 
+    // copia (move) dos valores da estrutura original
+    // ex: 
+    let frutas = vec!["banana","mamao","melao","abacate"];
+
+    // passagem por referencia
+    let numero_letras = frutas.iter().map(|fruta:&&str| fruta.len()).fold(0,|soma,len| soma+len);
+    println!("numero de letras no vec : {}",numero_letras);
+
+    // move do dado ( perde a ownership)
+    let numero_letras = frutas.into_iter().map(|fruta:&str| fruta.len()).fold(0,|soma,len| soma+len);
+    println!("numero de letras no vec : {}",numero_letras);
+    //println!("numnero_letras :{:?}",frutas); descomentar isso gera um erro (frutas ja nao tem mais o ownership do dado)
+
+    // MAPs, filters,  folds, etc...
+    // os iterators possuem funcoes que podem ser utilizadas em conjunto para os mais variados fins... ai vai um resumo
+    
+    //.map() -- aplica uma closure a cada elemento do vetor e retorna o resultado para a proxima funcao aplicada :
+    let vetor = vec![1,2,3,4,5];
+    println!("O VETOR -- {:?}",vetor);
+    // vamos usar o map para transformar todos os valores em valores pares
+    let vetor_pares:Vec<i32> = vetor.iter().map(|&x| x*2).collect();
+    println!("vetor pares : {:?}",vetor_pares);
+
+    
+    //filter() -- como o proprio nome sugere ele filtra os valores de acordo com a funcao closure passada para
+    // filtrando todos os elementos que nao sao pares
+    let vetor_impares:Vec<&i32> =vetor.iter().filter(|&x| x %2 !=0).collect();//.collect();
+    println!("vetor impares : {:?}",vetor_impares);
+    // como  uma pequena variacao podemos contar os numeros impares
+    let qtd_impares = vetor.iter().filter(|&x| x %2 !=0).count();
+    println!("numero de impares : {:?}",qtd_impares);
+
+    //fold() -- usa uma closure para acumular de alguma forma os elementos do vetor em uma unica linha vetor
+    let soma_elementos = vetor.iter().fold(0,|soma,x| soma+x);
+    println!("soma dos elementos do vetor {}",soma_elementos);
 
 
+    //zip() une 2 vetores
+    let index_vec = vec![0,1,2,3,4];
+    let vetor_tupla:Vec<(&i32,i32)> = vetor.iter().zip(index_vec).collect();
+    println!("vetor de tuplas {:?}",vetor_tupla);
 
-
-
-
-
-
+    // max() exibe o valor maximo de 1 vetor
+    let max_value = vetor.iter().max().unwrap();
+    println!("Valor maximo do vetor {:?}",max_value);
 
 
 }
